06c94464e0534f2aab0869163c00ec18adfbc169
Ali Ahmed <ali.nisar87@gmail.com>
2018-12-06 17:34:02 +0500

    finished

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import {
   json,
   geoAlbersUsa,
   geoPath,
-  scaleOrdinal,
   schemeGreens,
   extent,
   scaleQuantize,
   event,
-  scaleLinear
+  axisBottom,
+  zoom
 } from "d3";
 import { feature } from "topojson";
 
@@ -20,16 +20,25 @@ Promise.all([
     "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"
   )
 ]).then(([eduData, countryData]) => {
-  console.log("edu: ", eduData);
-  console.log("countryData: ", countryData);
+  // console.log("edu: ", eduData);
+  // console.log("countryData: ", countryData);
 
-  let counties = feature(countryData, countryData.objects.counties);
-  let states = feature(countryData, countryData.objects.states);
+  let counties = feature(countryData, countryData.objects.counties); // needed to draw counties borders
+  let states = feature(countryData, countryData.objects.states); // needed to draw a bit wider state borders
 
-  console.log(counties);
+  const numColors = 8; // number of colors in the fill scale
+  const stateBorderWidth = "1px"; // width of state borders
+  const stateBorderColor = "white";
+  const countyBorderWidth = "0.4px";
+  const countyBorderColor = "white";
+  const highlightColor = "black";
+  const highlightWidth = "0.8px";
 
-  counties.features.sort((a, b) => a.id - b.id);
+  // console.log(counties);
 
+  counties.features.sort((a, b) => a.id - b.id); // sorting from lowest to highest ids, so that it matches with eduData
+
+  // mapping eduData props onto counties.features
   counties.features.forEach((obj, i) => {
     obj.bachelors = eduData[i].bachelorsOrHigher;
     obj.state = eduData[i].state;
@@ -37,7 +46,7 @@ Promise.all([
     obj.fips = eduData[i].fips;
   });
 
-  console.log(counties);
+  // console.log(counties);
 
   // const projection = geoAlbersUsa();
   const pathGenerator1 = geoPath();
@@ -45,19 +54,19 @@ Promise.all([
   // .projection(projection);
 
   let [min, max] = extent(eduData, d => d.bachelorsOrHigher);
-  console.log(max, min);
-  let interval = (max - min) / 8;
-  console.log(interval);
+  // console.log(max, min);
+  let interval = (max - min) / numColors;
+  // console.log(interval);
 
-  let domain = new Array(8)
+  let domain = new Array(numColors)
     .fill(0)
     .map((elem, i) => Math.round(min + i * interval));
 
-  console.log(domain);
+  // console.log(domain);
 
   const colorScale = scaleQuantize()
-    .domain(extent(eduData, d => d.bachelorsOrHigher))
-    .range(schemeGreens[8]);
+    .domain(extent(domain))
+    .range(schemeGreens[numColors]);
 
   // Tooltip
 
@@ -72,28 +81,42 @@ Promise.all([
 
   const g = svg.append("g");
 
-  const pathOne = g.selectAll("path").data(counties.features);
-  const pathTwo = svg.selectAll("path").data(states.features);
+  const pathOne = g
+    .append("g")
+    .selectAll("path")
+    .data(counties.features); // for counties
+  const pathTwo = g
+    .append("g")
+    .selectAll("path")
+    .data(states.features); // for states
 
   pathTwo
     .enter()
     .append("path")
     .attr("d", pathGenerator2)
     .attr("fill", "none")
-    .attr("stroke", "white")
-    .attr("stroke-width", "1px");
+    .attr("stroke", stateBorderColor)
+    .attr("stroke-width", stateBorderWidth);
 
   const path = pathOne
     .enter()
     .append("path")
     .attr("class", "county")
     .attr("d", pathGenerator1)
-    .attr("stroke", "white")
-    .attr("stroke-width", "0.4px")
+    .attr("stroke", countyBorderColor)
+    .attr("stroke-width", countyBorderWidth)
     .attr("fill", d => colorScale(d.bachelors))
     .attr("data-fips", d => d.fips)
     .attr("data-education", d => d.bachelors);
   // console.log(path);
+
+  // zooming and panning
+
+  svg.call(
+    zoom().on("zoom", () => {
+      g.attr("transform", event.transform);
+    })
+  );
 
   // tooltip
 
@@ -105,8 +128,8 @@ Promise.all([
       .attr("data-education", d.bachelors)
       .html(`${d.area_name}, ${d.state}: ${d.bachelors}%`);
     select(this)
-      .attr("stroke", "black")
-      .attr("stroke-width", "0.8px");
+      .attr("stroke", highlightColor)
+      .attr("stroke-width", highlightWidth);
   });
 
   path.on("mouseleave", function(d) {
@@ -115,16 +138,17 @@ Promise.all([
       .style("left", 0)
       .style("top", 0);
     select(this)
-      .attr("stroke", "white")
-      .attr("stroke-width", "0.4px");
+      .attr("stroke", countyBorderColor)
+      .attr("stroke-width", countyBorderWidth);
   });
 
-  const legend = svg
-    .append("g")
-    .attr("transform", `translate(750, 20)`)
-    .attr("id", "legend");
+  const legend = svg.append("g");
 
-  legend
+  legend.attr("transform", `translate(650, 20)`).attr("id", "legend");
+
+  // console.log(legend);
+
+  const rect = legend
     .selectAll("rect")
     .data(colorScale.range())
     .enter()
@@ -133,4 +157,16 @@ Promise.all([
     .attr("x", (d, i) => i * 30)
     .attr("width", "30px")
     .attr("height", "10px");
+
+  let axis = axisBottom();
+
+  legend
+    .selectAll("text")
+    .data(domain)
+    .enter()
+    .append("text")
+    .attr("x", (d, i) => i * 30)
+    .attr("y", "23px")
+    .text(d => d + "%")
+    .attr("text-anchor", "middle");
 });
